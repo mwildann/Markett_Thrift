4f841ff58e6792a475e7aebc2945e90b233061e5
mwildann <muhammad.wildan23@students.utdi.ac.id>
2024-11-13 22:30:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Fungsi untuk membuka WhatsApp dengan nomor yang telah ditentukan
+function openWhatsApp() {
+    const phoneNumber = "6285845841748"; // Ganti dengan nomor WhatsApp Anda
+    window.open(`https://wa.me/${phoneNumber}`, '_blank');
+}
+
+// Fungsi untuk membuka Instagram
+function openInstagram() {
+    const instagramUsername = "Markett_Thrift"; // Ganti dengan username Instagram Anda
+    window.open(`https://instagram.com/${instagramUsername}`, '_blank');
+}
+
+// Fungsi untuk membuka TikTok
+function openTikTok() {
+    const tiktokUsername = "Markett_Thrift"; // Ganti dengan username TikTok Anda
+    window.open(`https://tiktok.com/@${tiktokUsername}`, '_blank');
+}
